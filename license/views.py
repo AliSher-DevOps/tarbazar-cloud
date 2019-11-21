5d55c11a6234f762c0b40039aec6245b0658c58f
IkramKhan-DevOps <57677370+IkramKhan-DevOps@users.noreply.github.com>
2019-11-21 22:10:19 +0500

    Update views.py

--- license/views.py
+++ license/views.py
@@ -1,19 +1,24 @@
+# REQUIRED-LIBRARIES
+# ------------------
+
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import (
     CreateAPIView, RetrieveAPIView, ListAPIView
 )
 from main.models import (
-    Organization, Frenchise, Business,
-    Country, City
+    Organization,
+    Business, Country, City,
+    SubModel, SubTime, Subscription
 )
 from .serializers import (
     SerCities, SeriCountries, SerBusinesses,
-    SeriOrganization, SeriFrenchise
+    SeriOrganization, SerSubModel, SerSubTime, SerSub
 )
 from .bll import LicenseSetup
 
 
-class LicenseOrgCreate(CreateAPIView):
+# ORGANIZATION ----------------------------
+class LicenseViewCreate_Org(CreateAPIView):
     queryset = Organization.objects.all()
     serializer_class = SeriOrganization
 
@@ -22,13 +27,13 @@ class LicenseOrgCreate(CreateAPIView):
         LicenseSetup.startfreelicense(data=serializer.data)
 
 
-class LicenseOrgRetrieve(RetrieveAPIView):
+class LicenseViewGet_Org(RetrieveAPIView):
     queryset = Organization.objects.all()
     serializer_class = SeriOrganization
     lookup_field = 'username'
 
 
-class LicenseGetBusinessAll(ListAPIView):
+class LicenseViewGets_Org(ListAPIView):
     serializer_class = SerBusinesses
 
     def get_queryset(self):
@@ -39,24 +44,63 @@ class LicenseGetBusinessAll(ListAPIView):
         return queryset_list
 
 
-class LicenseGetBusinessOne(RetrieveAPIView):
+# OTHER ---------------------------------------
+class LicenseViewGet_Business(RetrieveAPIView):
     queryset = Business.objects.all()
     serializer_class = SerBusinesses
     lookup_field = 'name'
 
 
-class LicenseGetCountries(ListAPIView):
+class LicenseViewGets_Countries(ListAPIView):
     queryset = Country.objects.all()
     serializer_class = SeriCountries
 
 
-class LicenseGetCities(ListAPIView):
+class LicenseViewGets_City(ListAPIView):
     queryset = City.objects.all()
     serializer_class = SerCities
 
     def get_queryset(self):
         queryset_list = City.objects.all()
-        query = self.request.GET.get("country")
+        query = self.request.GET.get("ser")
         if query:
             queryset_list = queryset_list.filter(country__name__iexact=query).distinct()
         return queryset_list
+
+
+# SUB-MODEL ---------------------------------
+class LicenseViewGets_SubModels(ListAPIView):
+    queryset = SubModel.objects.all()
+    serializer_class = SerSubModel
+
+
+class LicenseViewGet_SubModels(RetrieveAPIView):
+    queryset = SubModel.objects.all()
+    serializer_class = SerSubModel
+    lookup_field = 'name'
+
+
+# SUB-TIME ---------------------------------
+class LicenseViewGets_SubTime(ListAPIView):
+    queryset = SubTime.objects.all()
+    serializer_class = SerSubTime
+
+
+class LicenseViewGet_SubTime(RetrieveAPIView):
+    queryset = SubTime.objects.all()
+    serializer_class = SerSubTime
+    lookup_field = 'name'
+
+
+# SUB-SUB ----------------------------------
+class LicenseViewGets_Sub(ListAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SerSub
+
+
+class LicenseViewGet_Sub(RetrieveAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SerSub
+    lookup_field = 'name'
+
+
